9406166f864be5bccc671024442e900ea43d5295
VisualViolet <savannah_b_miller@outlook.com>
2023-01-08 11:43:25 -0800

    created function to add/remove classes based on time

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,27 +1,44 @@
-var currentDay = document.getElementById("currentDay");
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// wrapped to ensure code isn't run until HTML is rendered
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-    currentDay.textContent = dayjs().format("MMMM D, YYYY");
+  // when save button is clicked, the element id and block description are saved to local storage
+  $(".saveBtn").on("click", function () {
+    var hour = $(this).parent().attr("id");
+    var description = $(this).siblings(".description").val();
+    localStorage.setItem(hour, description);
+  });
 
+  // compares current time to the element time and applies or removes classes accordingly
+  function checkTime() {
+    var currentTime = parseInt(dayjs().format("H"));
+    $(".time-block").each(function () {
+      var elTime = parseInt($(this).attr("id").split("hour-").pop());
+      if (elTime < currentTime) {
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      } else if (elTime > currentTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      }
+    });
+  }
+
+  // retrieves local storage values and assigns them to the correct element using the element ID
+  function retrieveBlockItem() {
+    for (let hour = 9; hour <= 17; hour++) {
+      const elID = "hour-" + [hour];
+      var loopTime = localStorage.getItem(elID);
+      $("#" + elID + " .description").val(loopTime);
+    }
+  }
+  // displays current day to the page header
+  $("#currentDay").text(dayjs().format("MMMM D, YYYY"));
+  // function calls
+  checkTime();
+  retrieveBlockItem();
 });
